09c17dbb7c2a2758f8d1f744c1937ea12264991c
milena-mognon <milenamognon@gmail.com>
2020-09-28 16:56:39 -0300

    feat: initial config

---
+++ backend/src/shared/infra/http/server.ts
@@ -0,0 +1,30 @@
+import 'reflect-metadata';
+import express, { Request, Response, NextFunction } from 'express';
+import AppError from '@shared/error/AppError';
+
+const app = express();
+
+app.use(express.json());
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  // if is AppError answer with the correct status and message
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  // print the error on terminal
+  console.error(err);
+
+  // if an error happens without custom message, it answer with generic 500 error
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
+
+app.listen(3333, () => {
+  console.log('🚀 Server started on port 3333!');
+});
